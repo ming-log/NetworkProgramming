ed36aa9db77bc6ace885ce9d7131cca63859d1b5
骆明 <736704198@qq.com>
2020-09-01 10:32:19 +0800

    network

---
+++ socket的基本使用.py
@@ -0,0 +1,29 @@
+# 创建一个udp socket(udp套接字)
+import socket
+
+
+def main():
+	# 1. 创建udp的套接字
+	udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	# socket.AF_INET  使用ipv4协议
+	# socket.SOCK_DGRAM  指UDP
+
+	# ... 这里是使用套接字的功能...
+	# 2. 准备接收方的地址
+	# '192.168.1.103'标识目的ip地址
+	# 8080表示目的的端口
+	dest_addr = ('192.168.1.2', 8080)  # 注意 是元组， ip是字符串， 端口是数字
+
+	# 3. 从键盘获取数据
+	send_data = "hhhaaa111"
+
+	# 发送数据到指定的电脑上的指定程序中
+	udp_socket.sendto(send_data.encode('utf-8'), dest_addr)
+
+	# 关闭套接字
+	udp_socket.close()
+	print("---- run ----")
+
+
+if __name__ == "__main__":
+	main()
